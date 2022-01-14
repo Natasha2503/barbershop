f31cb201646d552315ef41e0670c726df8c7af9b
nzhmailova <nzhmailova@zuzex.com>
2022-01-14 15:22:05 +0300

    add new functions

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ from datetime import datetime, timedelta
 
 from flask import Flask, render_template, request
 
+from init_db import show_table
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'докторская колбаса'
 
-date_record = 0
-def create_dict_of_params(*args):
+name_hairdressers = 0
+date_and_time_record = 0
+
+def create_dict_of_params():
+    masters = show_table('masters')
+    masters_names = [masters[i][1] for i in range(len(masters))]
     data_about_time_workers = {}
     data_about_time_workers['datenow'] = datetime.today().strftime('%A %d %b')
     data_about_time_workers['timenow'] = datetime.today().strftime('%H:%M')
-    data_about_time_workers['names_hairdressers'] = args
+    data_about_time_workers['names_hairdressers'] = masters_names
     data_about_time_workers['numbers_of_workers'] = len(data_about_time_workers['names_hairdressers'])
     return data_about_time_workers
+
 
 def create_list_of_time(time_start_work):
     format_time_start_work = datetime.strptime(time_start_work, '%H:%M:%S.%f')
@@ -21,28 +28,26 @@ def create_list_of_time(time_start_work):
     return list_of_time
 
 
-
-@app.route('/',methods=['get','post'])
+@app.route('/')
 def main_web():
-    data_about_time_workers = create_dict_of_params('Alisa','Bob')
+    data_about_time_workers = create_dict_of_params()
     list_of_time = create_list_of_time('10:00:00.00')
-    if request.method == 'POST':
-        name_hairdressers = request.values.get('select_master')
-        date_record = request.values.get('date')
-        time_record = request.values.get('select_time')
-        format_time_record = datetime.strptime(time_record, '%H:%M:%S').time()
-        list_of_time.remove(format_time_record)
-        print(list_of_time)
-
-
     return render_template('haircutuser.html', data_about_time_workers=data_about_time_workers,
                            list_of_time=list_of_time)
 
-@app.route('/createrecord',methods = ['post'])
+
+@app.route('/createrecord', methods=['get', 'post'])
 def create_record():
-    datarecord = date_record
-    print(datarecord)
-    return render_template('record.html')
+    if request.method=='POST':
+        name_hairdressers = request.values.get('select_master')
+        date_and_time_record = ' '.join([request.values.get('date'), request.values.get('select_time')])
+        return render_template('record.html', name_hairdressers=name_hairdressers,
+                           date_and_time_record=date_and_time_record)
+    elif request.method =='GET':
+        name_G = request.form.get('nameclients')
+        print(name_G)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
