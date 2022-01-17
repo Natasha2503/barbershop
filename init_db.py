d2d58fc20cd6b48df4001ff51a302897174e79f1
nzhmailova <nzhmailova@zuzex.com>
2022-01-17 17:50:16 +0300

    class db

--- init_db.py
+++ init_db.py
@@ -1,19 +1,29 @@
 import psycopg2
+from psycopg2 import sql, Error
 from psycopg2.extras import DictCursor
-from psycopg2 import sql
 
-def connect_bd(dbname='barbershop', user='postgres',
-                            password='1524'):
-    conn = psycopg2.connect(dbname=dbname, user=user,
-                            password=password, host='localhost')
-    return conn
+from config import DataBaseConfig
+from log import LogError, LogInfo
+
+
+def connect_bd(dbname=DataBaseConfig.dbname, user=DataBaseConfig.user,
+               password=DataBaseConfig.password):
+    try:
+        conn = psycopg2.connect(dbname=dbname, user=user,
+                                password=password, host='localhost')
+        LogInfo.log.info("connect DB")
+        return conn
+    except (Exception, Error) as e:
+        LogError.log.error(e)
+
 
 def show_table(name_table):
     conn = connect_bd()
     with conn.cursor(cursor_factory=DictCursor) as cursor:
-        cursor.execute('SELECT * FROM '+ name_table)
+        cursor.execute('SELECT * FROM ' + name_table)
         table_data = cursor.fetchall()
     conn.close()
+    LogInfo.log.info("query is complited")
     return table_data
 
 
@@ -22,12 +32,14 @@ def add_to_masters(*args):
     with conn.cursor() as cursor:
         conn.autocommit = True
         values = [args
-        ]
+                  ]
         insert = sql.SQL('INSERT INTO masters (name) VALUES {}').format(
             sql.SQL(',').join(map(sql.Literal, values))
         )
         cursor.execute(insert)
     conn.close()
+    LogInfo.log.info("query is complited")
+
 
 def add_to_cliets(*args):
     conn = connect_bd()
@@ -39,11 +51,13 @@ def add_to_cliets(*args):
         values[-1] = name
         values = [tuple(values)]
         insert = sql.SQL(
-            'INSERT INTO clientsbarber (nameclient, telephone,date_of_record, id_master) VALUES {}').format(
+            'INSERT INTO clientsbarber (nameclient, telephone,dateofrecord, idmaster) VALUES {}').format(
             sql.SQL(',').join(map(sql.Literal, values))
         )
         cursor.execute(insert)
     conn.close()
+    LogInfo.log.info("query is complited")
+
 
 def delete_from_table(query):
     conn = connect_bd()
@@ -51,7 +65,27 @@ def delete_from_table(query):
         conn.autocommit = True
         cursor.execute(query)
     conn.close()
+    LogInfo.log.info("query is complited")
 
 
+def check_time_records(select_master):
+    conn = connect_bd()
+    with conn.cursor(cursor_factory=DictCursor) as cursor:
+        cursor.execute('select id from masters where name = %s', (select_master,))
+        id_master = cursor.fetchone()[0]
+        cursor.execute('SELECT dateofrecord FROM clientsbarber where idmaster = %s', (id_master,))
+        table_data = [row[0] for row in cursor]
+    conn.close()
+    LogInfo.log.info("query is complited")
+    return table_data
 
 
+def show_records_to_masters():
+    conn = connect_bd()
+    with conn.cursor(cursor_factory=DictCursor) as cursor:
+        cursor.execute(
+            'select c.nameclient,c.telephone,c.dateofrecord,m.name from clientsbarber as c inner join masters as m on c.idmaster=m.id')
+        table_data = cursor.fetchall()
+    conn.close()
+    LogInfo.log.info("query is complited")
+    return table_data
